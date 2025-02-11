92bed3334cac4ecaaa0fe0a3c5a41d99050abfa6
Kritawit.Ya <headerdev@gmail.com>
2025-02-11 23:18:47 +0700

    Initial commit

---
+++ src/test/java/com/idevalot/springboot_restful_webservices/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idevalot.springboot_restful_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
